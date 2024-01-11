f5152cd534c779fa683d80a9d7404cede841ea0c
ando <terbu102@gmail.com>
2024-01-11 21:05:36 +0300

    pycodestyle fix

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -3,8 +3,4 @@
 
 def floor(n: float) -> float:
     """return floored n"""
-    newn = int(n)
-    newn1 = float(newn + 0.50)
-    if n <= newn1:
-        return newn
-    return newn + 1
+    return floor(n)
