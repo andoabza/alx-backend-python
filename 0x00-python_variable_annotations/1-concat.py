51fd3b96eb699c56e9811c9cd14ca14a7ee049b0
ando <terbu102@gmail.com>
2024-01-11 08:38:22 +0300

    float to string type

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3 
 """return concatenated string from st1 and str2"""
 
-def concated(str1: str, str2: str) -> str:
+def concat(str1: str, str2: str) -> str:
     """return concat string"""
     return str1 + str2
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""float to string"""
+
+def to_str(n:float) -> str:
+    """to string"""
+    return str(n)
