864d169d8bf82eebe5c11c00fda34cd55f00f0aa
ando <terbu102@gmail.com>
2024-01-11 20:57:49 +0300

    flor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-import math
 """rerturn floor int"""
 
-def floor(n: int) -> int:
+def floor(n: float) -> float:
     """return floored n"""
-    return math.floor(n)
+    newn = int(n)
+    newn1 = float(newn + 0.50)
+    if n <= newn1:
+        return newn
+    return newn + 1
