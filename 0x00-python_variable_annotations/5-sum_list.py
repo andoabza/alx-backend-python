63bfcf2ed7e2fec7857a564986e5fdb901313099
ando <terbu102@gmail.com>
2024-01-11 08:52:20 +0300

    typing = List

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""floats sum"""
+
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """return sum of the list"""
+    sum = 0
+    for x in input_list:
+        sum += x
+    return sum
