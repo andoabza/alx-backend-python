b62d064a8f14cf6c6141a956ab082644ed30863d
ando <terbu102@gmail.com>
2024-01-15 15:24:48 +0300

    pycode

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """list of delay sec"""
-wait_random = __import__('0-basic_async_syntax').wait_random
 import asyncio
 import time
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> float:
