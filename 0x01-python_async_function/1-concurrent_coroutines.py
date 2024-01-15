10e9c958d56b765ca203c7ef44cd36df8ef0e960
ando <terbu102@gmail.com>
2024-01-15 16:03:11 +0300

    typing

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
-"""list of delay sec"""
+'''typing list
+'''
+import asyncio
+from typing import List
+
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-lock = asyncio.Lock()
-delays = []
-
-async def wait_n(n, max_delay):
-   global delays
-   delays = []
-   tasks = []
-   for _ in range(n):
-       task = asyncio.create_task(wait_random(max_delay))
-       tasks.append(task)
-   await asyncio.gather(*tasks)
-   return sorted(delays)
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''Executes wait_random n times.
+    '''
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
