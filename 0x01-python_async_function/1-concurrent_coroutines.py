3a9643e4b73f94c73201ebf7a695f3c561a8eae7
ando <terbu102@gmail.com>
2024-01-15 15:31:08 +0300

    concurrent

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,8 +8,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> float:
     """return list of floats"""
     result = []
-    delay = asyncio.create_task(wait_random(n * max_delay))
     for i in range(n):
-        delay = await delay
+        delay = await wait_random(max_delay)
         result.append(delay)
     return sorted(result)
