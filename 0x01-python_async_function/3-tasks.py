8d6791c9fd577d193a22446e12a2a22eea935dbb
ando <terbu102@gmail.com>
2024-01-15 19:51:13 +0300

    task sync function

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""return asyncio.task"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """return task"""
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
+
