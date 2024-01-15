e6d3dfb79ee52851a1bcaa795a0cb265fd8bdffe
ando <terbu102@gmail.com>
2024-01-15 15:45:09 +0300

    fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,9 +6,9 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> float:
-    """return list of floats"""
     result = []
-    for i in range(n):
-        delay = await wait_random(max_delay)
+    while n > 0:
+        delay = await wait_random(n * max_delay)
         result.append(delay)
-    return sorted(result)
+        n-1
+    return result
